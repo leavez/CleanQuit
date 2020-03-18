5a7dd3419e99107e523483194d50a1e2fdb26c58
leavez <gaojiji@gmail.com>
2020-03-18 12:01:41 +0800

    remove swiftshell

--- Package.swift
+++ Package.swift
@@ -13,14 +13,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueSignals.git", from: "1.0.21"),
-        .package(url: "https://github.com/kareman/SwiftShell", from: "5.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CleanQuit",
-            dependencies: ["Signals", "SwiftShell"]),
+            dependencies: ["Signals"]),
         .testTarget(
             name: "CleanQuitTests",
             dependencies: ["CleanQuit"]),
