f971ffd653bf553fea5346e112a7c613bd97b02c
leavez <gaojiji@gmail.com>
2021-02-04 18:20:30 +0800

    Use BFS search

--- Sources/CleanQuit/CleanQuit.swift
+++ Sources/CleanQuit/CleanQuit.swift
@@ -58,23 +58,29 @@ func _killAllChildrenProcesses(signal: Int32) {
     }
 }
 
-// find all children process ids recursively
+// Find all children process ids recursively.
+// The returned child process list is in BFS sequence of the process tree
 func findChildProcessIdsRecursively(pid: Int32) -> [Int32] {
     
-    func findRecursively(pid: String) -> [String] {
-        var out = [String]()
+    func findChildren(pid: String) -> [String] {
         let result = exec("/usr/bin/pgrep", args: ["-P", pid])
-        if result.code == 0 {
-            let childIds = result.output.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: "\n").map(String.init)
-            out += childIds
-            for c in childIds {
-                out += findRecursively(pid: c)
-            }
+        if result.code != 0 {
+            return []
         }
-        return out
+        return result.output.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: "\n").map(String.init)
+    }
+    var fifo = ["\(pid)"]
+    var out: [String] = []
+    while !fifo.isEmpty {
+        let head = fifo[0]
+        fifo = Array(fifo.dropFirst())
+        let children = findChildren(pid: head)
+        fifo += children
+        out += children
     }
     
-    return findRecursively(pid: "\(pid)").compactMap(Int32.init)
+    return out.compactMap(Int32.init)
+        .filter({ $0 != 0 }) // filter 0 as it has a special meaning for `kill`. It won't result a 0, just for insurance
 }
 
 func exec(_ executablePath: String, args: [String]) -> (code: Int32, output:String) {
